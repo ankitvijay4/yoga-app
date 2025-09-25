427208117bcf2a50ca23b9d98a7d4260bc882e01
ankitvijay4 <ankitvijaykansal@gmail.com>
2025-09-25 16:59:56 -0500

    chore: initial commit - scaffold yoga-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import YogaTrainer from './YogaTrainer'; // Import the new YogaTrainer component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Your Yoga Trainer</h1>
+      <YogaTrainer /> {/* Use the YogaTrainer component */}
     </div>
   );
 }
